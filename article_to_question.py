61237417fc65965665b0f6db7c5cc9b5f7464073
Lawrence Wu <lawrence0510@gmail.com>
2023-10-15 15:03:14 +0800

    文章->輸出問題 自動化流程完成
    輸入：article.txt
    執行：article_to_question.py
    輸出：output.txt

---
+++ article_to_question.py
@@ -0,0 +1,34 @@
+import os
+import openai
+
+openai.api_key = f'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'
+
+def article_to_question(text):
+    messages = [
+        {"role": "system", "content": "你是一名專門在閱讀文章後產生問題給學生回答的得力助手。"},
+        {"role": "user", "content": text + "\n 根據以上文本，請使用繁體中文提供三個關於此文本的問題，以及答案和詳細解釋。"},
+    ]
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+
+    answer = response['choices'][0]['message']['content']
+    return answer
+
+def main():
+    with open('article.txt', 'r') as file:
+        article_text = file.read()
+
+    result = article_to_question(article_text)
+
+    with open('output.txt', 'w') as output_file:
+        output_file.write(result)
+
+    print("已輸出至output.txt")
+
+if __name__ == "__main__":
+    main()
+
+
