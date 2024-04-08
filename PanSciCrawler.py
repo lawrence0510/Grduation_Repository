bf8210efc90b852fc6c94a4ee4de8608e51a3cce
Lawrence Wu <lawrence0510@gmail.com>
2024-04-08 22:43:20 +0800

    泛科學單篇文章爬蟲完成

---
+++ PanSciCrawler.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://pansci.asia/archives/374794'
+response = requests.get(url)
+response.encoding = 'utf-8'
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+title = soup.find('title')
+print('標題：'+title.text+'\n\n')
+
+paragraphs_text = []
+paragraphs = soup.find_all('p')
+for paragraph in paragraphs:
+    paragraphs_text.append(paragraph.text)
+
+paragraphs_text = paragraphs_text[4:]
+
+updated_paragraphs_text = []
+for paragraph in paragraphs_text:
+    if '討論功能關閉中。' in paragraph:
+        break
+    else:
+        updated_paragraphs_text.append(paragraph)
+
+final_text = '\n'.join(updated_paragraphs_text)
+
+print('內文：\n'+final_text)
