ffa5a1ff98b8e751e38b46841ddcdce817ea9ea1
Lawrence Wu <lawrence0510@gmail.com>
2024-04-20 14:29:15 +0800

    新增文章->問題的api/刪除不必要的檔案

--- api.py
+++ api.py
@@ -1,18 +1,19 @@
+from openai import OpenAI
 from flask import Flask, session, jsonify
 from flask_restx import Api, Resource, reqparse
 import mysql.connector
 from mysql.connector import Error
 import hashlib
-import openai
 from datetime import datetime, timedelta
 from werkzeug.datastructures import FileStorage
 import pandas as pd
+import json
 
-
-openai.api_key = f'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'
 app = Flask(__name__)
 app.secret_key = '5b52b65660fc4c498fe0ed356fdc5212'
-api = Api(app, version='1.0', title='Graduation_Repository APIs', description='110級畢業專案第一組\n組員：吳堃豪、侯程麟、謝佳蓉、許馨文、王暐睿\n指導教授：洪智鐸')
+api = Api(app, version='1.0', title='Graduation_Repository APIs',
+          description='110級畢業專案第一組\n組員：吳堃豪、侯程麟、謝佳蓉、許馨文、王暐睿\n指導教授：洪智鐸')
+
 
 def create_db_connection():
     try:
@@ -27,55 +28,77 @@ def create_db_connection():
         print(f"Error: '{e}'")
         return None
 
+
 user_parser = reqparse.RequestParser()
 user_parser.add_argument('user_name', type=str, required=True, help='使用者名稱')
-user_parser.add_argument('user_password', type=str, required=True, help='使用者密碼')
+user_parser.add_argument('user_password', type=str,
+                         required=True, help='使用者密碼')
 user_parser.add_argument('user_school', type=str, required=True, help='使用者學校')
 user_parser.add_argument('user_age', type=int, required=True, help='使用者年齡')
-user_parser.add_argument('user_email', type=str, required=True, help='使用者email')
+user_parser.add_argument('user_email', type=str,
+                         required=True, help='使用者email')
 user_parser.add_argument('user_phone', type=str, required=True, help='使用者電話')
 
 login_parser = reqparse.RequestParser()
 login_parser.add_argument('user_name', type=str, required=True, help='使用者名稱')
-login_parser.add_argument('user_password', type=str, required=True, help='使用者密碼')
+login_parser.add_argument('user_password', type=str,
+                          required=True, help='使用者密碼')
 
 user_id_parser = reqparse.RequestParser()
 user_id_parser.add_argument('user_id', type=int, required=True, help='使用者ID')
 
 email_reset_parser = reqparse.RequestParser()
-email_reset_parser.add_argument('original_email', type=str, required=True, help='使用者舊email')
-email_reset_parser.add_argument('original_password', type=str, required=True, help='使用者密碼')
-email_reset_parser.add_argument('new_email', type=str, required=True, help='使用者新email')
+email_reset_parser.add_argument(
+    'original_email', type=str, required=True, help='使用者舊email')
+email_reset_parser.add_argument(
+    'original_password', type=str, required=True, help='使用者密碼')
+email_reset_parser.add_argument(
+    'new_email', type=str, required=True, help='使用者新email')
 
 password_reset_parser = reqparse.RequestParser()
-password_reset_parser.add_argument('user_email', type=str, required=True, help='使用者email')
-password_reset_parser.add_argument('original_password', type=str, required=True, help='使用者舊密碼')
-password_reset_parser.add_argument('new_password', type=str, required=True, help='使用者新密碼')
+password_reset_parser.add_argument(
+    'user_email', type=str, required=True, help='使用者email')
+password_reset_parser.add_argument(
+    'original_password', type=str, required=True, help='使用者舊密碼')
+password_reset_parser.add_argument(
+    'new_password', type=str, required=True, help='使用者新密碼')
 
 phone_reset_parser = reqparse.RequestParser()
-phone_reset_parser.add_argument('user_email', type=str, required=True, help='使用者email')
-phone_reset_parser.add_argument('user_password', type=str, required=True, help='使用者密碼')
-phone_reset_parser.add_argument('new_phone', type=str, required=True, help='使用者新電話號碼')
+phone_reset_parser.add_argument(
+    'user_email', type=str, required=True, help='使用者email')
+phone_reset_parser.add_argument(
+    'user_password', type=str, required=True, help='使用者密碼')
+phone_reset_parser.add_argument(
+    'new_phone', type=str, required=True, help='使用者新電話號碼')
 
 name_reset_parser = reqparse.RequestParser()
-name_reset_parser.add_argument('user_email', type=str, required=True, help='使用者email')
-name_reset_parser.add_argument('user_password', type=str, required=True, help='使用者密碼')
-name_reset_parser.add_argument('new_name', type=str, required=True, help='使用者新名稱')
+name_reset_parser.add_argument(
+    'user_email', type=str, required=True, help='使用者email')
+name_reset_parser.add_argument(
+    'user_password', type=str, required=True, help='使用者密碼')
+name_reset_parser.add_argument(
+    'new_name', type=str, required=True, help='使用者新名稱')
 
 school_reset_parser = reqparse.RequestParser()
-school_reset_parser.add_argument('user_email', type=str, required=True, help='使用者email')
-school_reset_parser.add_argument('user_password', type=str, required=True, help='使用者密碼')
-school_reset_parser.add_argument('new_school', type=str, required=True, help='新學校名稱')
+school_reset_parser.add_argument(
+    'user_email', type=str, required=True, help='使用者email')
+school_reset_parser.add_argument(
+    'user_password', type=str, required=True, help='使用者密碼')
+school_reset_parser.add_argument(
+    'new_school', type=str, required=True, help='新學校名稱')
 
 article_upload_parser = reqparse.RequestParser()
-article_upload_parser.add_argument('file', type=FileStorage, location='files', required=True, help='可以上傳Excel檔案，格式要是|article_title|article_link|article_content|')
+article_upload_parser.add_argument('file', type=FileStorage, location='files',
+                                   required=True, help='可以上傳Excel檔案，格式要是|article_title|article_link|article_content|')
 
 get_questions_parser = reqparse.RequestParser()
-get_questions_parser.add_argument('article_text', type=str, required=True, help='欲產生問題之文章內文')
+get_questions_parser.add_argument(
+    'article_text', type=str, required=True, help='欲產生問題之文章內文')
 
 
-#User api區
+# User api區
 user_ns = api.namespace('User', description='與使用者操作相關之api')
+
 
 @user_ns.route('/get_all_user')
 class DataList(Resource):
@@ -92,6 +115,7 @@ class DataList(Resource):
         else:
             return {"error": "Unable to connect to the database"}, 500
 
+
 @user_ns.route('/register')
 class RegisterUser(Resource):
     @user_ns.expect(user_parser)
@@ -104,7 +128,7 @@ class RegisterUser(Resource):
         user_age = args['user_age']
         user_email = args['user_email']
         user_phone = args['user_phone']
-        
+
         # 使用 SHA-256 對密碼加密
         encrypted_password = hashlib.sha256(user_password.encode()).hexdigest()
 
@@ -120,7 +144,8 @@ class RegisterUser(Resource):
                 INSERT INTO `User`(`user_id`, `user_name`, `user_password`, `user_school`, `user_age`, `user_email`, `user_phone`)
                 VALUES (%s, %s, %s, %s, %s, %s, %s)
                 """
-                cursor.execute(sql, (new_user_id, user_name, encrypted_password, user_school, user_age, user_email, user_phone))
+                cursor.execute(sql, (new_user_id, user_name, encrypted_password,
+                               user_school, user_age, user_email, user_phone))
                 connection.commit()
                 return {"message": "User registered successfully"}, 201
             except Error as e:
@@ -131,6 +156,7 @@ class RegisterUser(Resource):
         else:
             return {"error": "Unable to connect to the database"}, 500
 
+
 @user_ns.route('/login')
 class LoginUser(Resource):
     @user_ns.expect(login_parser)
@@ -139,7 +165,7 @@ class LoginUser(Resource):
         args = login_parser.parse_args()
         user_name = args['user_name']
         user_password = args['user_password']
-        
+
         encrypted_password = hashlib.sha256(user_password.encode()).hexdigest()
 
         connection = create_db_connection()
@@ -152,7 +178,7 @@ class LoginUser(Resource):
                 """
                 cursor.execute(sql, (user_name, encrypted_password))
                 user = cursor.fetchone()
-                
+
                 if user:
                     session['user_id'] = user['user_id']
                     return {"message": "Login successful", "user_id": user['user_id']}, 200
@@ -166,12 +192,14 @@ class LoginUser(Resource):
         else:
             return {"error": "Unable to connect to the database"}, 500
 
+
 @user_ns.route('/logout')
 class LogoutUser(Resource):
     def post(self):
         '''登出用戶'''
         session.pop('user_id', None)
         return {"message": "You have been logged out."}, 200
+
 
 @user_ns.route('/get_user_from_id')
 class GetUserFromID(Resource):
@@ -180,7 +208,7 @@ class GetUserFromID(Resource):
         '''根據 user_id 獲取用戶資料'''
         args = user_id_parser.parse_args()
         user_id = args['user_id']
-        
+
         connection = create_db_connection()
         if connection is not None:
             try:
@@ -188,7 +216,7 @@ class GetUserFromID(Resource):
                 sql = "SELECT * FROM `User` WHERE `user_id` = %s"
                 cursor.execute(sql, (user_id,))
                 user = cursor.fetchone()
-                
+
                 if user:
                     return user, 200
                 else:
@@ -200,6 +228,7 @@ class GetUserFromID(Resource):
                 connection.close()
         else:
             return {"error": "Unable to connect to the database"}, 500
+
 
 @user_ns.route('/reset_email')
 class EmailReset(Resource):
@@ -217,11 +246,13 @@ class EmailReset(Resource):
             try:
                 cursor = connection.cursor()
                 select_sql = "SELECT `user_id` FROM `User` WHERE `user_email` = %s AND `user_password` = %s"
-                cursor.execute(select_sql, (original_email, encrypted_password))
+                cursor.execute(
+                    select_sql, (original_email, encrypted_password))
                 user = cursor.fetchone()
                 if user:
                     update_sql = "UPDATE `User` SET `user_email` = %s WHERE `user_email` = %s AND `user_password` = %s"
-                    cursor.execute(update_sql, (new_email, original_email, encrypted_password))
+                    cursor.execute(
+                        update_sql, (new_email, original_email, encrypted_password))
                     connection.commit()
                     if cursor.rowcount > 0:
                         return {"message": "Email updated successfully"}, 200
@@ -237,6 +268,7 @@ class EmailReset(Resource):
         else:
             return {"error": "Unable to connect to the database"}, 500
 
+
 @user_ns.route('/reset_password')
 class PasswordReset(Resource):
     @user_ns.expect(password_reset_parser)
@@ -246,8 +278,10 @@ class PasswordReset(Resource):
         user_email = args['user_email']
         original_password = args['original_password']
         new_password = args['new_password']
-        encrypted_original_password = hashlib.sha256(original_password.encode()).hexdigest()
-        encrypted_new_password = hashlib.sha256(new_password.encode()).hexdigest()
+        encrypted_original_password = hashlib.sha256(
+            original_password.encode()).hexdigest()
+        encrypted_new_password = hashlib.sha256(
+            new_password.encode()).hexdigest()
 
         connection = create_db_connection()
         if connection is not None:
@@ -258,7 +292,8 @@ class PasswordReset(Resource):
                 user = cursor.fetchone()
                 if user:
                     update_sql = "UPDATE `User` SET `user_password` = %s WHERE `user_email` = %s"
-                    cursor.execute(update_sql, (encrypted_new_password, user_email))
+                    cursor.execute(
+                        update_sql, (encrypted_new_password, user_email))
                     connection.commit()
                     return {"message": "Password updated successfully"}, 200
                 else:
@@ -270,6 +305,7 @@ class PasswordReset(Resource):
                 connection.close()
         else:
             return {"error": "Unable to connect to the database"}, 500
+
 
 @user_ns.route('/reset_phone')
 class PhoneReset(Resource):
@@ -287,7 +323,8 @@ class PhoneReset(Resource):
             try:
                 cursor = connection.cursor()
                 update_sql = "UPDATE `User` SET `user_phone` = %s WHERE `user_email` = %s AND `user_password` = %s"
-                cursor.execute(update_sql, (new_phone, user_email, encrypted_password))
+                cursor.execute(
+                    update_sql, (new_phone, user_email, encrypted_password))
                 if cursor.rowcount == 0:
                     return {"message": "Invalid email or password"}, 404
                 else:
@@ -300,6 +337,7 @@ class PhoneReset(Resource):
                 connection.close()
         else:
             return {"error": "Unable to connect to the database"}, 500
+
 
 @user_ns.route('/reset_name')
 class NameReset(Resource):
@@ -317,7 +355,8 @@ class NameReset(Resource):
             try:
                 cursor = connection.cursor()
                 update_sql = "UPDATE `User` SET `user_name` = %s WHERE `user_email` = %s AND `user_password` = %s"
-                cursor.execute(update_sql, (new_name, user_email, encrypted_password))
+                cursor.execute(
+                    update_sql, (new_name, user_email, encrypted_password))
                 if cursor.rowcount == 0:
                     return {"message": "Invalid email or password"}, 404
                 else:
@@ -330,6 +369,7 @@ class NameReset(Resource):
                 connection.close()
         else:
             return {"error": "Unable to connect to the database"}, 500
+
 
 @user_ns.route('/reset_school')
 class SchoolReset(Resource):
@@ -347,7 +387,8 @@ class SchoolReset(Resource):
             try:
                 cursor = connection.cursor()
                 update_sql = "UPDATE `User` SET `user_school` = %s WHERE `user_email` = %s AND `user_password` = %s"
-                cursor.execute(update_sql, (new_school, user_email, encrypted_password))
+                cursor.execute(
+                    update_sql, (new_school, user_email, encrypted_password))
                 if cursor.rowcount == 0:
                     return {"message": "Invalid email or password"}, 404
                 else:
@@ -361,8 +402,10 @@ class SchoolReset(Resource):
         else:
             return {"error": "Unable to connect to the database"}, 500
 
-#Article api區
+
+# Article api區
 article_ns = api.namespace('Article', description='與文章操作相關之api')
+
 
 @article_ns.route('/get_all_article')
 class DataList(Resource):
@@ -377,7 +420,8 @@ class DataList(Resource):
                 # 將所有datetime對象轉換為字符串
                 for article in articles:
                     if isinstance(article['article_expired_day'], datetime):
-                        article['article_expired_day'] = article['article_expired_day'].strftime('%Y-%m-%d')
+                        article['article_expired_day'] = article['article_expired_day'].strftime(
+                            '%Y-%m-%d')
                 return jsonify(articles)
             except Error as e:
                 return {"error": str(e)}, 500
@@ -387,6 +431,7 @@ class DataList(Resource):
         else:
             return {"error": "Unable to connect to the database"}, 500
 
+
 def get_max_article_id(connection):
     cursor = connection.cursor()
     cursor.execute("SELECT MAX(article_id) FROM `Article`")
@@ -394,6 +439,7 @@ def get_max_article_id(connection):
     max_id = result[0] if result[0] is not None else 0
     cursor.close()
     return max_id
+
 
 @article_ns.route('/upload_articles')
 class UploadArticles(Resource):
@@ -415,7 +461,8 @@ class UploadArticles(Resource):
                     article_title = row['article_title']
                     article_link = row['article_link']
                     article_content = row['article_content']
-                    article_expired_day = (datetime.now() + timedelta(days=60)).strftime('%Y-%m-%d')
+                    article_expired_day = (
+                        datetime.now() + timedelta(days=60)).strftime('%Y-%m-%d')
 
                     try:
                         cursor = connection.cursor()
@@ -443,6 +490,7 @@ class UploadArticles(Resource):
         else:
             return {"error": "No file uploaded"}, 400
 
+
 @article_ns.route('/upload_pansci')
 class UploadPanSciArticles(Resource):
     def post(self):
@@ -461,8 +509,9 @@ class UploadPanSciArticles(Resource):
                 article_link = item['article_link']
                 article_content = item['article_content']
                 article_category = item['article_category']
-                article_expired_day = (datetime.now() + timedelta(days=60)).strftime('%Y-%m-%d')
-                
+                article_expired_day = (
+                    datetime.now() + timedelta(days=60)).strftime('%Y-%m-%d')
+
                 try:
                     cursor = connection.cursor()
                     insert_query = """
@@ -486,14 +535,68 @@ class UploadPanSciArticles(Resource):
         else:
             return {"error": "Unable to connect to the database"}, 500
 
-#OpenAI api區
-openAI_ns = api.namespace('OpenAI', description='與openai操作相關之api，‼️此區皆為付費區，需測試請先洽Lawrence‼️')
+
+# OpenAI api區
+openAI_ns = api.namespace(
+    'OpenAI', description='與openai操作相關之api，‼️此區皆為付費區，需測試請先洽Lawrence‼️')
+OpenAI.api_key = f'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'
 
 @openAI_ns.route('/get_questions_from_article')
 class GetQuestionsFromArticle(Resource):
     @openAI_ns.expect(get_questions_parser)
     def post(self):
-        '''傳送文章內容至openai API，獲得三個問題及標準答案'''
+        '''傳送文章內容至OpenAI API，獲得三個問題及標準答案'''
+        args = get_questions_parser.parse_args()
+        article = args['article_text']
+        client = OpenAI(
+            api_key='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')
+
+        try:
+            prompt_message = (
+                f"以下是一篇文章的內容：\n{article}\n\n"
+                "請根據以上文本，follow這個格式使用繁體中文回傳給我，"
+                "我想要你首先判斷這篇文章的適讀年齡，要回傳一個阿拉伯數字放入article_age中，"
+                "介於6到15歲之間，再回傳三個問題，第一二題都是選擇題，分別要提供四個選項，"
+                "再提供答案和解析，第三題要是問答題，你可以先產生一個標準答案並儲存在answer3中，"
+                "格式如下：" + json.dumps({
+                    "article_age": None,
+                    "question1": None,
+                    "question1choice1": None,
+                    "question1choice2": None,
+                    "question1choice3": None,
+                    "question1choice4": None,
+                    "answer1": None,
+                    "explanation1": None,
+                    "question2": None,
+                    "question2choice1": None,
+                    "question2choice2": None,
+                    "question2choice3": None,
+                    "question2choice4": None,
+                    "answer2": None,
+                    "explanation2": None,
+                    "question3": None,
+                    "answer3": None
+                })
+            )
+            
+            response = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": "你是一名專門在閱讀文章後產生問題給學生回答的得力助手。"},
+                    {"role": "user", "content": prompt_message}
+                ],
+            )
+
+            response_str = str(response).replace('\n', '').replace('\\n', '')
+            content_start = response_str.find("content='") + len("content='")
+            content_end = response_str.find("}', role='assistant'", content_start) + 1
+            content_json_str = response_str[content_start:content_end]
+            content_json_str = content_json_str.replace("\\'", "'").replace('\\"', '"').replace('\\\\', '\\')
+            content_json = json.loads(content_json_str)
+
+            return content_json, 200
+        except Exception as e:
+            return {'error': str(e)}, 500
 
 
 
