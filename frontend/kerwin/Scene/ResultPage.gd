8d509da8696aef0d46ac087592f7ed40807ebc71
kerwinTheCoder <54coderkwjui@gmail.com>
2024-08-04 21:19:12 +0800

    dialog

--- frontend/kerwin/Scene/ResultPage.gd
+++ frontend/kerwin/Scene/ResultPage.gd
@@ -1,10 +1,7 @@
 extends Control
 
 func _ready():
-	pass # Replace with function body.
-
-
+	pass
 
 func _on_Timer_timeout():
-	$MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer2/ProgressBar.value += 1
-
+	$MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer2/HBoxContainer2/ProgressBar.value += 1
--- frontend/kerwin/Scene/UserProgress.gd
+++ frontend/kerwin/Scene/UserProgress.gd
@@ -1,2 +1,7 @@
 extends KinematicBody2D
 
+var move = Vector2(3, 0)
+
+func _physics_process(delta):
+	var collision = move_and_collide(move)
+
