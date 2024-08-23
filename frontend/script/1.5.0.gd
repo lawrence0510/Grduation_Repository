13e02f0f7b1c0118893c9fc724c208bbde121c66
kerwinTheCoder <54coderkwjui@gmail.com>
2024-08-23 17:50:15 +0800

    1.5.0測試文章顯示成功

--- frontend/script/1.5.0.gd
+++ frontend/script/1.5.0.gd
@@ -5,9 +5,22 @@ onready var wave1: Button = $bg/wave1
 onready var wave2: Button = $bg/wave2
 onready var wave3: Button = $bg/wave3
 onready var score: Button = $bg/score
+onready var http_request: HTTPRequest = $HTTPRequest
 
 func _ready() -> void:
-	pass
+	var url = "http://140.119.19.145:5001/Article/get_random_article"
+	
+	# 建立 POST 請求的資料
+	var data = {
+		"article_id": 30,
+		"answer": 20,
+	}
+	
+	var json_data = JSON.print(data)
+	var headers = ["Content-Type: application/json"]
+	# 發送HTTP GET請求
+	http_request.request(url, headers, true, HTTPClient.METHOD_GET)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -33,3 +46,10 @@ func _on_cross_pressed():
 
 func _on_score_pressed():
 	get_tree().change_scene("res://scene/1.5.4.tscn")
+
+
+
+func _on_HTTPRequest_request_completed(result, response_code, headers, body):
+	var json = JSON.parse(body.get_string_from_utf8())
+	#print(json.result[0].article_content)
+	$bg/content2.text = json.result[0].article_content
