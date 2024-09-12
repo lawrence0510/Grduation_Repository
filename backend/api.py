653694c3aa38e28602dc1511a50773aa7ee9aa10
lawrence0510 <lawrence0510@gmail.com>
2024-09-12 15:53:15 +0800

    google verification api fixed

--- backend/api.py
+++ backend/api.py
@@ -343,7 +343,7 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = '110306047@g.nccu.edu.tw'
-app.config['MAIL_PASSWORD'] = 'sdpbnrynypmufpsj'
+app.config['MAIL_PASSWORD'] = 'ahwesebrfcsdrvkl'
 app.config['MAIL_DEFAULT_SENDER'] = ('Reading King', 'reading@gmail.com')
 
 mail = Mail(app)
