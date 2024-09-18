d443334b74d97a6b4199c3335f54d769ec8fb83d
wenwen2003 <110306002@nccu.edu.tw>
2024-09-18 22:05:13 +0800

    修1.5後的模板(待微調)

---
+++ frontend/script/1.4.5.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		# 如果遊戲當前是全螢幕模式，則退出全螢幕
+		if OS.window_fullscreen:
+			get_tree().quit()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_cross_pressed():
+	get_tree().change_scene("res://scene/1.4.4.tscn")
