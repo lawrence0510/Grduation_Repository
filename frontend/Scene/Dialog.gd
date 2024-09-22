12abd630c46c5be1e570821bfbfde0246864de5f
kerwinTheCoder <54coderkwjui@gmail.com>
2024-09-23 01:00:07 +0800

    9/23 1:00 追問介面api串聯完成，等api做好就可以實際問答

--- frontend/Scene/Dialog.gd
+++ frontend/Scene/Dialog.gd
@@ -18,6 +18,11 @@ func _ready():
 	var starting_message = Response.instance()
 	starting_message.text = "Hello challenger, if there's anything you want to ask me?"
 	add_response_to_game(starting_message)
+	
+	#設定我等待ai回應時間
+	$Timer.wait_time = 0.2
+	#只等一次
+	$Timer.one_shot = true
 
 #追蹤新輸入文字，自動下拉對話視窗
 func handle_scrollbar_changed():
@@ -29,14 +34,20 @@ func _on_Input_text_entered(new_text):
 	if new_text.empty():
 		return
 	var input_response = InputResponse.instance()
-	var ai_response = AIResponse.instance()
+
 	input_response.set_text(new_text)
 	add_response_to_game(input_response)
 	#控制回復
 	var ai_response_text = command_processor.process_command(new_text)
-	ai_response.set_text(GlobalVar.aiResponse)
-	add_response_to_game(ai_response)
-		
+	$Timer.start()
+
 
 func add_response_to_game(response: Control):
 	history_rows.add_child(response)
+
+#在等待兩秒之後, 把ai的回應設給ai_response這個node的文字, 再把那個node加到history_rows裡面
+func _on_Timer_timeout():
+	print("timeout")
+	var ai_response = AIResponse.instance()
+	ai_response.set_text(GlobalVar.aiResponse)
+	add_response_to_game(ai_response)
