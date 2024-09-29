126baea9a2c02ae16033ce6aa79c7c6e88067c79
wenwen2003 <110306002@nccu.edu.tw>
2024-09-29 11:42:13 +0800

    9/29更新Main

--- frontend/script/1.4.2_10.gd
+++ frontend/script/1.4.2_10.gd
@@ -15,7 +15,7 @@ func _ready() -> void:
 		labels.append(label)
 	
 	# 定義要變成按鈕的label號碼，假設是 3, 11 和 28
-	var label_numbers = [10, 20, 30]
+	var label_numbers = []
 	
 	# 將這些指定的label變成button
 	for number in label_numbers:
