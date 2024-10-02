18b58db16dc7b615f4e78cf1a7b265c4ce46da40
lawrence0510 <lawrence0510@gmail.com>
2024-10-02 22:46:20 +0800

    暫停畫面確定退出連結

--- frontend/BattleSystem/ConfirmEndingScene.gd
+++ frontend/BattleSystem/ConfirmEndingScene.gd
@@ -4,3 +4,7 @@ extends ColorRect
 ## 取消button按下去
 func _on_CancelButton_pressed() -> void:
 	self.visible = false ## 隱藏確認離開的場景
+
+
+func _on_ConfirmEndingButton_pressed():
+	get_tree().change_scene("res://Scene/MainPage.tscn")
